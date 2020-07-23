e6d00898aec95e238b84f573e0bbfe283459cca6
vorSherer <tcpma2011@gmail.com>
2020-07-22 19:25:15 -0700

    Scraper.py imports and BeautifulSoup beginnings.

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -1,6 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
+URL = 'https://en.wikipedia.org/wiki/NASA'
+
+response = requests.get(URL)
+# print(response)
+
+content = response.content
+soup = BeautifulSoup(content, "html.parser")
+# print(soup.prettify())
+
+result = soup.find(class='mw-parser-output')
+print(result.length())
+
+
 def get_citations_needed_count(URL):
     pass
 
