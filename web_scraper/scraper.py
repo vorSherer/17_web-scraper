e150adc44b7c316ef03b07f44cb4dfddab789966
vorSherer <tcpma2011@gmail.com>
2020-07-22 21:42:20 -0700

    Finished get_citations_needed_count function, modularized soup code to DRY up the module, began get_citations_needed_report function.

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -3,26 +3,52 @@ from bs4 import BeautifulSoup
 
 URL = 'https://en.wikipedia.org/wiki/NASA'
 
-response = requests.get(URL)
-# print(response)
 
-content = response.content
-soup = BeautifulSoup(content, "html.parser")
-# print(soup.prettify())
+def drill_down(URL):
+    response = requests.get(URL)
+    # print(response)
 
-result = soup.find(class='mw-parser-output')
-print(result.length())
+    soup = BeautifulSoup(response.content, "html.parser")
+    # print(soup.prettify())
+
+    body = soup.find('body')
+    # print(body)
+
+    body_content = soup.find(id='content')
+    # print(body_content)
+
+    actual_body_content = soup.find(id='bodyContent')
+    # print(actual_body_content)
+
+    mw_content = soup.find(id='mw-content-text')
+    # print(mw_content)
+
+    mw_parser = soup.find(class_='mw-parser-output')
+    # print(mw_parser)
+
+    cite_reqs = soup.find_all(class_='noprint Inline-Template Template-Fact')
+    # print(cite_reqs)
+    # print(len(cite_reqs))   # Returned correct count of citations needed on page
+    return cite_reqs
 
 
 def get_citations_needed_count(URL):
-    pass
+    cite_reqs = drill_down(URL)
+    return len(cite_reqs)
 
 
 def get_citations_needed_report(URL):
-    pass
+    cite_report = drill_down(URL)
+    citation_text = [c.parent.text.replace('[citation needed]', '') for c in cite_report]
+    print(citation_text)
+    # Use list and string methods to break out the citation_text list into individual strings.
+
 
 
 
 
 if __name__ == "__main__":
-    pass
+    URL = 'https://en.wikipedia.org/wiki/NASA'
+    print(get_citations_needed_count(URL))
+    print(get_citations_needed_report(URL))
+
